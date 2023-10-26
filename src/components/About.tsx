91ce5e09048a07475df7d44941c468dfcccc72d8
zero_jeden <zeroitjeden@gmail.com>
2023-10-26 23:32:54 +0200

    add button Back in subpages About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,11 @@
+import Back from "./Back";
 
 export default function About() {
 return (
     <div>
     <h1>About</h1>
     <p>...this is project funpage Star Wars about learning JS ;</p>
+    <Back/>
     </div>
 );
 }
