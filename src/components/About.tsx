1321c45c467de91164614305533fe431c8b7970b
zero_jeden <zeroitjeden@gmail.com>
2023-11-13 21:31:15 +0100

    refresh About (test)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,14 @@
-import Back from "./Back";
-import Header from "./Header";
+import Back from './Back';
+import Header from './Header';
 
 export default function About() {
-return (
-    <div>
-    <Header sections={[]} title={''} />
-    <h1>About</h1>
-    <p>...this is project funpage Star Wars about learning JS ;</p>
-    <Back/>
-    </div>
-);
+	return (
+		<div>
+			<Header sections={[]} title={''} />
+			<h1>About</h1>
+			<p>...this is project funpage Star Wars about learning JS ;</p>
+			<p> test test</p><br/>
+			<Back />
+		</div>
+	);
 }
